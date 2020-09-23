1efc3cdeebf65405671c7aff4356d04f199665d6
Joe Day <jday4@nd.edu>
2020-09-23 12:22:56 -0400

    Removed an error and added clarity

--- solution_part2.sh
+++ solution_part2.sh
@@ -1,15 +1,15 @@
 #Run this script with bash this_file_name wages.csv
 #Returns the highest Earner
 
-echo The highest earner:
+echo The highest earner demographics in gender, years of experience, wage:
 
-sort -n -t ',' -k4 $1 | tail -n 1
+sort -n -t ',' -k4 $1 | tail -n 1 | cut -d , -f 1,2,4
 
 #Returns the Lowest Earner
 
-echo The lowest earner:
+echo The lowest earner demographics in gender, years of experience, wage:
 
-sort -n -t ',' -k4 $1 | head -n 2 | tail -n 1
+sort -n -t ',' -k4 $1 | head -n 2 | tail -n 1 | cut -d , -f 1,2,4
 
 #Returns the the number of females in the top ten earners in this data set
  
